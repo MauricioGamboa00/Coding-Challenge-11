b6499581d4bd9be6b7cd9da6367666580011fee7
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-02 13:24:28 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -24,3 +24,29 @@ book1.updateCopies(-1); // Reduce book copies by 1
 
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
 
+// Task 2 Creating a Borrower Class
+
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name; // Borrower Name
+        this.borrowerId = borrowerId; // Borrower ID
+        this.borrowedBooks = []; // Array of Borrowed Books
+    }
+
+    borrowBook(book) {
+        this.borrowedBooks.push(book); // Adding Books to the borrowed Books array
+    }
+
+    returnBook(book) {
+     this.borrowedBooks = this.borrowedBooks.filter(b => b !== book); // Removing Book from borrowed Books array
+    }
+}; // Created Borrower Class
+
+const borrower1 = new Borrower("Alice Johnson", 201); // Added a new Borrower
+borrower1.borrowBook("The Great Gatsby"); // Borrower 1 borrowing Great Gatsby
+
+console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby"); // Borrower 1 returning the Great Gatsby
+
+console.log(borrower1.borrowedBooks); // Expected output: []
